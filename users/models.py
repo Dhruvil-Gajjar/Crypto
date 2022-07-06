7d6a6c8181d228a7bea607ec316683ffea4d17a8
Dhruvil-Gajjar <dhruvilvi.dev@gmail.com>
2022-07-06 14:48:45 +0530

    Added new required fields in user model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.base_user import BaseUserManager
@@ -38,12 +39,36 @@ class UserProfileManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
+COMPANY_SIZE_CHOICES = (
+    (1, _("0 - 9")),
+    (2, _("10 - 49")),
+    (3, _("50 - 249")),
+    (4, _("250 - 499")),
+    (5, _("500 - 999")),
+    (6, _("1000+"))
+)
+
+SECTOR_CHOICES = (
+    (1, _("Drop 1")),
+    (2, _("Drop 2"))
+)
+
+PHONE_REGEX = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+)
+
+
 class User(AbstractUser):
     id = models.CharField(primary_key=True, max_length=36, editable=False, default=uuid.uuid4)
     username = None
     email = models.EmailField(_('email address'), unique=True)
+    company_name = models.CharField(max_length=256, blank=True, null=True,
+                                    validators=[RegexValidator('[+-/%]', inverse_match=True)])
+    company_size = models.IntegerField(choices=COMPANY_SIZE_CHOICES, default=1)
+    sector = models.IntegerField(choices=SECTOR_CHOICES, default=1)
     date_of_birth = models.DateTimeField(blank=True, null=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(validators=[PHONE_REGEX], max_length=20, blank=True, null=True)
     free_trial = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
