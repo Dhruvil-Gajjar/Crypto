3da5f81f7782c2bd4071657041bb5b058724b185
Dhruvil-Gajjar <dhruvilvi.dev@gmail.com>
2022-07-23 12:56:09 +0530

    Added footer changes

--- subscription/views.py
+++ subscription/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http.response import HttpResponseRedirect, JsonResponse, HttpResponse
 from django.views.generic import ListView, CreateView, DetailView, TemplateView
@@ -53,7 +54,7 @@ def stripe_config(request):
 def create_checkout_session(request):
     if request.method == 'POST':
         stripe_product_id = request.POST.get('stripeProductId')
-        domain_url = 'http://52.8.38.105/'
+        domain_url = get_current_site(request).domain
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
             checkout_session = stripe.checkout.Session.create(
